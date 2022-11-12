a81881bda7f5dd3f06afbf94dc124445f1b0a6ac
sergei270791 <sergei.calle.c@uni.pe>
2022-11-12 18:03:32 -0500

    Primera mejora a fase 3

---
+++ ExamenParcial-3S2/Pregunta2/src/test/java/Fase3/AirportTest.java
@@ -0,0 +1,87 @@
+package Fase3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AirportTest {
+
+    @DisplayName("Dado que hay un vuelo economico")
+    @Nested
+    class EconomyFlightTest {
+
+        private Flight economyFlight;
+
+        @BeforeEach
+        void setUp() {
+            economyFlight = new EconomyFlight("1");
+        }
+
+        @Test
+        public void testEconomyFlightRegularPassenger() {
+            Passenger jessica = new Passenger("Jessica", false);
+
+            assertAll(()->assertEquals("1", economyFlight.getId()),
+                ()->assertEquals(true, economyFlight.addPassenger(jessica)),
+                ()->assertEquals(1, economyFlight.getPassengersList().size()),
+                ()->assertEquals("Jessica", economyFlight.getPassengersList().get(0).getName()),
+
+                ()->assertEquals(true, economyFlight.removePassenger(jessica)),
+                ()->assertEquals(0, economyFlight.getPassengersList().size()));
+        }
+
+        @Test
+        public void testEconomyFlightVipPassenger() {
+            Passenger cesar = new Passenger("Cesar", true);
+
+            assertAll(()->assertEquals("1", economyFlight.getId()),
+                ()->assertEquals(true, economyFlight.addPassenger(cesar)),
+                ()->assertEquals(1, economyFlight.getPassengersList().size()),
+                ()->assertEquals("Cesar", economyFlight.getPassengersList().get(0).getName()),
+
+                ()->assertEquals(false, economyFlight.removePassenger(cesar)),
+                ()->assertEquals(1, economyFlight.getPassengersList().size()));
+
+        }
+
+    }
+
+    @DisplayName("Dado que hay un vuelo negocios")
+    @Nested
+    class BusinessFlightTest {
+        private Flight businessFlight;
+
+        @BeforeEach
+        void setUp() {
+            businessFlight = new BusinessFlight("2");
+        }
+
+        @Test
+        public void testBusinessFlightRegularPassenger() {
+            Passenger jessica = new Passenger("Jessica", false);
+
+            assertAll(()->assertEquals(false, businessFlight.addPassenger(jessica)),
+            ()->assertEquals(0, businessFlight.getPassengersList().size()),
+            ()->assertEquals(false, businessFlight.removePassenger(jessica)),
+            ()->assertEquals(0, businessFlight.getPassengersList().size()));
+
+        }
+
+        @Test
+        public void testBusinessFlightVipPassenger() {
+            Passenger cesar = new Passenger("Cesar", true);
+
+            assertAll(()->assertEquals(true, businessFlight.addPassenger(cesar)),
+            ()->assertEquals(1, businessFlight.getPassengersList().size()),
+            ()->assertEquals(false, businessFlight.removePassenger(cesar)),
+            ()->assertEquals(1, businessFlight.getPassengersList().size()));
+        }
+
+    }
+
+}
+
